2a44e89a39a83bb3e1500a5f712580eddf0ec3bc
Ezra Magaram <magaram@usc.edu>
2023-04-28 02:08:22 +0200

    Initial Commit

---
+++ SuperLaunch/SuperLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperLaunchApp.swift
+//  SuperLaunch
+//
+//  Created by Ezra Magaram on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
